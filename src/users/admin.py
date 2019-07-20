e96eb24c04951695c515039adbc1f867d4614b3c
JamieCzuy <jamie.czuy@gmail.com>
2019-07-20 11:12:20 -0400

    Step #6 - Create Custom User Model

--- src/users/admin.py
+++ src/users/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from django.contrib import auth
 
-# Register your models here.
+from users.forms import UserChangeForm
+from users.forms import UserCreationForm
+from users.models import User
+
+
+@admin.register(User)
+class UserAdmin(auth.admin.UserAdmin):
+    add_form = UserChangeForm
+    form = UserCreationForm
+    list_display = ["email", "username"]
